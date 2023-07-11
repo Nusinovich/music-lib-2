360251052e8d1039669eb2fa653a0e59c0da8b9d
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-07-10 19:53:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useEffect, useState, useRef } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom'
+import HomePage from './components/HomePage.js'
+import ArtistView from './components/ArtistView'
+import AlbumView from './components/AlbumView'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+  let searchInput = useRef('');
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    const fetchData = async () => {
+        document.title = `${term} Music`
+        const response = await fetch(API_URL + term)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+            return setData(resData.results)
+        } else {
+            return setMessage('Not Found.')
+        }
+    }
+    fetchData()
+}
+
+console.log('DATA FROM API!!! app.js', data)
+  return (
+      <div className='App'>
+        <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
+          <DataContext.Provider value={data}>
+
+            <Router>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/artist/:id" element={<ArtistView />} />
+                <Route path="/album/:id" element={<AlbumView />} />
+              </Routes>
+            </Router>
+            
+          </DataContext.Provider>
+        </SearchContext.Provider>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Gallery.js
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import GalleryItem from './GalleryItem'
+import { DataContext } from '../context/DataContext'
+
+
+const Gallery = () => {
+    const data = useContext(DataContext)
+    console.log('Data in gallery!!', data)
+
+    const display = data.map((item, index) => {
+        return (
+            <GalleryItem key={index} item={item} />
+        )
+    })
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery
